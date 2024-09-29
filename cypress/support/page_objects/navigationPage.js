79c3cd3aa2bc40bcffbc2af0f741a3372d303675
Mohammed N <mnaser11218@gmail.com>
2024-09-28 22:23:54 -0400

    reformated navigationPage class and seperated functions to keep code dry

--- cypress/support/page_objects/navigationPage.js
+++ cypress/support/page_objects/navigationPage.js
@@ -1,20 +1,26 @@
+const selectGroupMenuItem = (groupName) =>{
+
+    cy.contains('a', groupName).then(menu=>{
+        cy.wrap(menu).find('[class="expand-state"] g g').invoke('attr', 'data-name').then(attr=>{
+            if(attr.includes('left')){
+                cy.wrap(menu).click()
+            }
+        })
+    })
+
+}
+
 export class NavigationPage{
 
+   
+
     formLayoutPage(){
-        cy.contains('Forms').click()
+        selectGroupMenuItem('Form')
         cy.contains('Form Layouts').click()
     }
     datePickerPage(){
-        
-        cy.get('[class="ng-tns-c7-4 ng-star-inserted active"]').then(menu=>{
-            cy.wrap(menu).find('[class="expand-state"] g g').invoke('attr', 'data-name').then(attr=>{
-                if(attr.includes('left')){
-                    cy.wrap(menu).click()
-                }
-            })
-        })
-
-       // cy.contains('Forms').click()
+ selectGroupMenuItem('Form')
+      // cy.contains('Forms').click()
         cy.contains('Datepicker').click()
         }
     

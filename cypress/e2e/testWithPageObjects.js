b537d3af3bb8012460a5aa33711f584eb99b5e59
Mohammed N <mnaser11218@gmail.com>
2024-09-30 12:52:36 -0400

    added method submitInlineFormwithnameandemail in formLayoutpage class to test inline form inputs  submit

--- cypress/e2e/testWithPageObjects.js
+++ cypress/e2e/testWithPageObjects.js
@@ -1,16 +1,21 @@
+import { onFormLayoutsPage } from "../support/page_objects/formLayoutsPage"
 import { navigateTo } from "../support/page_objects/navigationPage"
 
 describe('Test with page objects', ()=>{
     beforeEach('open application', ()=>{
         cy.visit('/')
     })
-    it.only('verify navigations across pages', ()=>{
+    it('verify navigations across pages', ()=>{
         navigateTo.formLayoutPage()
         navigateTo.datePickerPage()
         navigateTo.smartTable()
         navigateTo.toastrPage()
         navigateTo.toolTipPage()
         navigateTo.dialogPage()
+    })
+
+    it.only('should submit Inline and Basic form and select tomorrow date in the calendar', ()=> {
+        onFormLayoutsPage.submitInlineFormWithNameAndEmail()
     })
 
 })

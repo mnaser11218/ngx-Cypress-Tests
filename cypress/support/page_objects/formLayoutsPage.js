20ec271f821c89a3c947ff4c7e270c2cc2ca2578
Mohammed N <mnaser11218@gmail.com>
2024-09-30 14:09:43 -0400

    added form tag in submitinline method

--- cypress/support/page_objects/formLayoutsPage.js
+++ cypress/support/page_objects/formLayoutsPage.js
@@ -5,7 +5,7 @@ export class FormLayoutsPage{
 submitInlineFormWithNameAndEmail(){
     console.log("inside method")
     navigateTo.formLayoutPage()
-    cy.contains('nb-card',  'Inline form').then(nbCard=>{
+    cy.contains('nb-card',  'Inline form').find('form').then(nbCard=>{
         cy.wrap(nbCard).find('[placeholder="Jane Doe"]').type('testing')
         cy.wrap(nbCard).find('[placeholder="Email"]').type('testing@gmail.com')
         cy.wrap(nbCard).find('[type="submit"]').click()

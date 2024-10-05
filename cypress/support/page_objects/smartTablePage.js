4e1554e6c4c8fc3f6e9c37affdaa0d740633a039
Mohammed N <mnaser11218@gmail.com>
2024-10-04 23:22:43 -0400

    completed addUserToTable method in smartTable class

--- cypress/support/page_objects/smartTablePage.js
+++ cypress/support/page_objects/smartTablePage.js
@@ -1,7 +1,12 @@
 export class SmartTable {
 
     addUserToTable(user){
-        
+        cy.get('tbody').contains('tr', 'Jacob').then(tableRow=>{
+            cy.wrap(tableRow).find('.nb-edit').click()
+            cy.wrap(tableRow).find('[placeholder="Age"]').clear().type("50")
+            cy.wrap(tableRow).find('.nb-checkmark').click()
+            cy.wrap(tableRow).find('td').eq(6).should('contain', "50")
+        })
     }
 
 }
